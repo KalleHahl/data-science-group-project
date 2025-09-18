6aa60b4d0db172f63e3a0b204229df64ccf1220a
Sami Kazan <sami.kazan@helsinki.fi>
2025-09-18 16:26:05 +0300

    minor fix for sql table

--- init.sql
+++ init.sql
@@ -5,8 +5,9 @@ CREATE DATABASE wine_db;
 CREATE TABLE wines (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    price INTEGER,
+    price FLOAT,
     rating FLOAT,
+    year VARCHAR(100),
     goes_with TEXT[],
     descript VARCHAR(100)
 );
